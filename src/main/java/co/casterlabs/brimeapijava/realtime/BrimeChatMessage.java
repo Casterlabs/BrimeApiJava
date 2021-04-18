af404ee9afdcb6c3b00e2af5564202aa31af287e
Ender <33337309+e3ndr@users.noreply.github.com>
2021-04-17 22:40:21 -0500

    Add Brime emotes (1.7.0)

--- src/main/java/co/casterlabs/brimeapijava/realtime/BrimeChatMessage.java
+++ src/main/java/co/casterlabs/brimeapijava/realtime/BrimeChatMessage.java
@@ -1,5 +1,8 @@
 package co.casterlabs.brimeapijava.realtime;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.google.gson.annotations.SerializedName;
 
 import co.casterlabs.brimeapijava.types.BrimeUser;
@@ -11,9 +14,32 @@ import lombok.ToString;
 @ToString
 @AllArgsConstructor
 public class BrimeChatMessage {
+    private static final String IMAGE_URL_FORMAT = "https://content.brimecdn.com/brime/emote/%s/%s";
+
     @SerializedName("channelID")
     private String channelId;
     private String message;
     private BrimeUser sender;
+    private Map<String, BrimeChatEmote> emotes = new HashMap<>();
+
+    @Getter
+    @ToString
+    public static class BrimeChatEmote {
+        @SerializedName("_id")
+        private String emoteId;
+
+        public String get1xImageUrl() {
+            return String.format(IMAGE_URL_FORMAT, this.emoteId, "1x");
+        }
+
+        public String get2xImageUrl() {
+            return String.format(IMAGE_URL_FORMAT, this.emoteId, "2x");
+        }
+
+        public String get3xImageUrl() {
+            return String.format(IMAGE_URL_FORMAT, this.emoteId, "3x");
+        }
+
+    }
 
 }
